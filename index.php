be54f472a520404a1477e64b8ddaa8d4526d9c19
Rudie Dirkx <github@hotblocks.nl>
2025-04-06 21:55:34 +0200

    +zbateson/mail-mime-parser meh

--- index.php
+++ index.php
@@ -2,16 +2,21 @@
 
 use PhpMimeMailParser\Attachment;
 use PhpMimeMailParser\Parser;
+use ZBateson\MailMimeParser\Header\IHeader;
+use ZBateson\MailMimeParser\Header\IHeaderPart;
+use ZBateson\MailMimeParser\Message;
+use ZBateson\MailMimeParser\Message\MimePart;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $emlFiles = glob(__DIR__ . '/eml/*.eml');
 // dump($emlFiles);
 $emlFile = $emlFiles[array_rand($emlFiles)];
-// $emlFile = 'eml/Warning message 1tWLyC-00A3ZR-If delayed 72 hours.eml';
-// $emlFile = 'eml/Undeliverable FW New reservation.eml';
+// $emlFile = 'eml/Undeliverable Nieuwe reservering (20-01-2025 om 16 30 op Padel indoor 2) ...464854.eml';
 dump($emlFile);
 $emlContent = file_get_contents($emlFile);
+
+echo "\n<hr>\n\n";
 
 class MyParser extends Parser {
 	public function getAttachmentsOfTypes(array $types) : array {
@@ -28,6 +33,35 @@ class MyParser extends Parser {
 	}
 }
 
+
+$message = Message::from($emlContent, false);
+// dump($message->getAllHeadersByName('to'));
+dump($message->getHeaderValue('subject'));
+$relevantParts = $message->getAllParts(function(MimePart $part) {
+	return in_array($part->getContentType(), ['text/rfc822-headers', 'message/rfc822']);
+});
+dump($relevantParts);
+
+foreach ($relevantParts as $part) {
+	$subMessage = Message::from($part->getContent(), false);
+	dump(
+		$part->getContentType(),
+		array_map(fn($hd) => $hd->getValue(), $subMessage->getAllHeadersByName('to')),
+		$subMessage->getAllHeadersByName('to'),
+		array_map(function(array $map) : null|string|array {
+			return count($map) > 1 ? $map : $map[0];
+		}, array_reduce($subMessage->getAllHeaders(), function(array $map, IHeader $hd) : array {
+			$map[strtolower($hd->getName())][] = implode(', ', array_map(fn(IHeaderPart $p) => $p->getValue(), $hd->getParts()));
+			return $map;
+		}, [])),
+		// array_combine(array_column($hds = $subMessage->getRawHeaders(), 0), array_column($hds, 1)),
+	);
+}
+
+
+echo "\n<hr>\n\n";
+
+
 $parser = MyParser::fromText($emlContent);
 dump($parser->getHeader('subject'));
 // dump($parser->getMessageBody());
@@ -35,9 +69,17 @@ dump($parser->getHeader('subject'));
 dump(array_map(fn(array $part) => $part['headers']['content-type'] ?? '?', $parser->getParts()));
 $relevantParts = $parser->getAttachmentsOfTypes(['text/rfc822-headers', 'message/rfc822']);
 dump($relevantParts);
+
 foreach ($relevantParts as $part) {
-	$subparser = MyParser::fromText($part->getContent());
-	dump($part->getContentType(), array_column($subparser->getAddresses('to'), 'address'), $subparser->getHeaders());
+	$subParser = MyParser::fromText($part->getContent());
+	dump(
+		$part->getContentType(),
+		array_column($subParser->getAddresses('to'), 'address'),
+		$subParser->getHeaders(),
+	);
 }
+
+
+echo "\n<hr>\n\n";
 
 dump($emlContent);
